897656c9c186caf0ac65c608345d84a67b4efce2
Kamil Babayev <kamilyunis@gmail.com>
2024-01-14 21:31:07 +0400

    console program example

--- console.cpp
+++ console.cpp
@@ -8,14 +8,15 @@ int main() {
     double server_model;
 
     cout << "enter server name: ";
-    cin >> server_name;
+    cin >> server_name;  // get server name
 
     cout << "enter server ip address: ";
-    cin >> server_ip;
+    cin >> server_ip;    // get server ip
 
     cout << "enter server model: ";
     cin >> server_model;
 
+    // print all input information
     cout << endl << "--------------------" << endl
          << "server name: " << server_name << endl
          << "server ip: " << server_ip << endl
